338a54ec8dea8fe0bc6214b07a631e5b5ce19c6b
Vladimir Shutov <v@smd.su>
2018-03-04 22:49:32 +0400

    6

--- Sources/SwiftyBot/main.swift
+++ Sources/SwiftyBot/main.swift
@@ -90,7 +90,7 @@ droplet.get("info") { _ in
     return "SwiftyBot did start"
 }
 
-droplet.post(telegramSecret) { request in
+droplet.post("telegram", telegramSecret) { request in
     
     print("request \(request)")
     
