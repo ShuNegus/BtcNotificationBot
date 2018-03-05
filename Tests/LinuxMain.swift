e422d21025a4bed7bf58f6d9934a07c6cbdb2c2d
Vladimir Shutov <v@smd.su>
2018-03-05 22:23:43 +0400

    7

--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import ZEGBotTests
-
-XCTMain([
-	testCase(ZEGBotTests.allTests),
-	])
--- docker/test.sh
+++
@@ -1,7 +0,0 @@
-sudo docker run \
---rm \
--v `pwd`/:/ZEGBot \
--w /ZEGBot \
-swiftdocker/swift:latest \
-/bin/sh -c \
-"swift test"
